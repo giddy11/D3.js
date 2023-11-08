2467ffa866bf673010d36b8c80279983def327dc
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-11-08 23:20:19 +0100

    updated

--- Paulo_Practice/script.js
+++ Paulo_Practice/script.js
@@ -1,12 +1,12 @@
 const canvas = d3.select(".canva");
 
-var dataArray = [
-    {width: 25, height: 4, fill: "pink"},
-    {width: 25, height: 14, fill: "purple"},
-    {width: 25, height: 44, fill: "orange"},
-    {width: 25, height: 124, fill: "green"},
-    {width: 25, height: 12, fill: "grey"}
-];
+// var dataArray = [
+//     {width: 25, height: 4, fill: "pink"},
+//     {width: 25, height: 14, fill: "purple"},
+//     {width: 25, height: 44, fill: "orange"},
+//     {width: 25, height: 124, fill: "green"},
+//     {width: 25, height: 12, fill: "grey"}
+// ];
 
 // const svg = canvas.select("svg")
 const svg = canvas.append("svg")
@@ -15,12 +15,31 @@ const svg = canvas.append("svg")
 
 const rect = svg.selectAll("rect");
 
-d3.json("https://jsonplaceholder.typicode.com/todos/1")
+// d3.json("https://jsonplaceholder.typicode.com/todos/1")
+d3.json("text.json")
     .then(data => {
         console.log(data);
-    })
 
-rect.data(dataArray)
+// rect.data(dataArray)
+//     .enter()
+//     .append("rect")
+//     .attr("width", 24)
+//     // .attr("fill", function(d) {
+//     //     return d.fill
+//     // })
+//     .attr("fill", d => d.fill)
+//     .attr("height", function(d) {
+//         return d.height * 2
+//     })
+//     // .attr("x", function(d,i) {
+//     //     return i * 25
+//     // })
+//     .attr("x", (d, i) => i * 25)
+//     .attr("y", function(d,i) {
+//         return 100 - (d.height * 2)
+//     })
+
+rect.data(data)
     .enter()
     .append("rect")
     .attr("width", 24)
@@ -36,6 +55,7 @@ rect.data(dataArray)
     // })
     .attr("x", (d, i) => i * 25)
     .attr("y", function(d,i) {
-        return 100 - (d.height * 2)
+        return 200 - (d.height * 2)
     })
 
+})
